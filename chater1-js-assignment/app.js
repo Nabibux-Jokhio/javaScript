614921035aa0a2df35ab7683393dd5c99caa093d
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-02-27 22:13:53 +0500

    Add files via upload

---
+++ chater1-js-assignment/app.js
@@ -0,0 +1,24 @@
+// chapter 1 assignment
+// Q1:
+
+// alert('hello everyone')
+
+// Q2:
+
+// document.write("<h1>holle world</h1>")
+
+// Q3:
+
+// document.write("<p>welcome to js land <br/> Happy Coding</p>")
+
+// Q4:
+
+// alert('welcome to js land')
+// alert(' Happy Coding')
+
+// Q5:
+
+// console.log("hello... I can through my web brower's console");
+
+
+
