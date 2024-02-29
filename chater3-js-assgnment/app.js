2669a331da1c2da94caf45f446b30d124bafa602
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-01 00:00:33 +0500

    Add files via upload

---
+++ chater3-js-assgnment/app.js
@@ -0,0 +1,43 @@
+// chatper 3
+// assignment variables for numbers
+
+
+// Q1:
+var age;
+
+age=25
+
+alert(age)
+
+// Q2:
+
+
+
+
+
+
+
+
+
+
+// Q3:
+var birthYear;
+birthYear = 1997
+
+document.write(birthYear, "<br/>")
+document.write("The type of this variable is ",  typeof(birthYear))
+
+
+// Q4:
+
+var customerName= 'ali'
+var product = 'gifts'
+var quantity = 6
+
+document.write(customerName , ' ordered ', quantity ,' ' , product , ' on ABC.Com' )
+
+
+
+
+
+
