12d04d258dfdac6f62cdbc46f0f257eb4dcc016c
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-02-29 23:09:16 +0500

    Add files via upload

---
+++ chater2-js-assgnment/app.js
@@ -0,0 +1,75 @@
+// chatper 2
+// assignment variables for string
+
+
+// Q1:
+var userName;
+
+// Q2:
+
+var myName = 'Nabi Bux Jokhio'
+
+
+
+// Q3:
+
+var message;
+
+message = 'Hello world'
+
+
+alert(message)
+
+// Q4:
+
+var name = 'Nabi Bux Johkio'
+var dateofbirth= '23-03-1997'
+var courseEnrolled = 'wed and mobile app develop'
+
+alert(name)
+alert(dateofbirth)
+alert(courseEnrolled)
+
+// Q5:
+
+
+var food1 = 'PIZZA'
+var food2 = 'PIZZ'
+var food3 = 'PIZ'
+var food4 = 'PI'
+var food5 = 'P'
+
+alert(food1+'\n'+food2+'\n'+food3+'\n'+food4+'\n'+food5 )
+
+
+
+// Q6:
+
+
+ var email = 'jokhionb12@gmail.com'
+
+ alert('My Email Addree is  ' +email)
+
+
+// Q7:
+
+var book;
+
+book = 'A smarter way to learn JavaScript.'
+
+alert(book)
+
+// Q8:
+
+var para = 'i display this on wb page'
+
+document.write(para)
+
+
+// Q9:
+
+var newString = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+
+alert(newString)
+document.write(newString)
+
