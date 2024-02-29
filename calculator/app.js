ca02e13eaaca6e945336da8eed812fc2a2b03ab8
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-01 00:18:12 +0500

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,33 @@
+
+// assignment of calculator
+
+
+// Q1:
+
+var num1 = +prompt("Enter the number 1")
+var num2 = +prompt("Enter the number 2")
+
+
+// addition
+
+var sum = num1+num2
+
+
+// subtraction
+
+var sub  = num1-num2;
+
+// multiple
+
+var mul = num1*num2
+
+// divide
+
+var div = num1/num2
+
+// addition
+
+var remainder = num1%num2
+
+
+alert('The sum of ' +num1 +' + ' +num2 +' is ' +sum +"\n" + 'The sum of ' +num1 +' - ' +num2 +' is ' +sub +"\n" + 'The sum of ' +num1 +' * ' +num2 +' is ' +mul +"\n" +'The sum of ' +num1 +' / ' +num2 +' is ' +div +"\n" +'The sum of ' +num1 +' % ' +num2 +' is ' +remainder )
