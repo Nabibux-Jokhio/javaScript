739575a441673ca625c3e1d2a4f1ae75f0942bc5
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-07 12:09:33 -0800

    Add files via upload

---
+++ chater4-js-assgnment/app.js
@@ -0,0 +1,62 @@
+// chatper 4
+// assignment VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+// Q1:
+let name;
+let num;
+let Email;
+
+// Q2:
+
+// LEGAL
+let $name1;
+let _name;
+let name123;
+let userName;
+let user_name;
+
+// ILLEGAL
+
+// let 123name;
+// let prompt;
+// let @number;
+// let ~userName;
+// let ++sum;
+
+
+
+
+
+
+// Q3:
+
+let letter= "letter"
+let number= "number"
+let $name = "$"
+let _uesrName =" _"
+
+let $name2 ="$name"
+let _name1 ="_name"
+var name1 ="name"
+
+let camelCase = "camelCase"
+
+let js = "keywords"
+
+
+
+document.write("<h1>Rules for naming JS variables</h1>"  , "<br/>")
+document.write("Variable names can only contain  ",letter , " " , number," ", $name," ", "and"," ",_uesrName  , "<br/>")
+document.write("Variables must begin with a ",$name2 , " " , _name1," ", "and"," ",name1  , "<br/>")
+document.write("Variable names are case: ",camelCase, " " ,  "<br/>")
+document.write("Variable names should not be JS ",js, " " , "<br/>")
+
+
+
+
+
+
+
+
+
