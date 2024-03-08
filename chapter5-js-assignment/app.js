29e47717e3ace9dca3d6eb6c442a83a160fa4ad3
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-08 17:32:12 +0500

    Add files via upload

---
+++ chapter5-js-assignment/app.js
@@ -0,0 +1,211 @@
+// Chapter 5
+// Assignment of MATH EXPRESSIONS
+
+// Q1:
+
+// let num1 = 3
+// let num2 = 8
+// let sum = num1 + num2
+
+
+// document.write( '<p>'+ "sum of " +num1 +" "+"and" +" "+ num2 +" "+ "is" + " "+sum + '</p>');
+
+
+// Q2:
+
+
+// let sub =num2-num1
+// let mul =num2*num1
+// let div =num2/num1
+// let rem =num2%num1
+
+// document.write('<p>'+"sub of " +num2 +" "+"and" +" "+ num1 +" "+ "is" + " "+ sub + '</p>' );
+// document.write( '<p>'+"multi of " +num1 +" "+"and" +" "+ num2 +" "+ "is" + " "+mul + '</p>');
+// document.write( '<p>'+"divison of " +num2 +" "+"and" +" "+ num1 +" "+ "is" + " "+div + '</p>' );
+// document.write( '<p>'+"remainder of " +num2 +" "+"and" +" "+ num1 +" "+ "is" + " "+rem + '</p>');
+
+
+// Q3:
+
+// let num;
+
+// document.write('<p>'+"Veriable is "+ num+ '</p>')
+
+// num = 5
+// document.write('<p>'+"Intial Value:"+ num+ '</p>')
+
+// let increment =++num 
+// document.write('<p>'+"Value after Increment:"+ increment+ '</p>')
+
+// let add = increment +7
+// document.write('<p>'+"Add + 7  is :"+ add+ '</p>')
+// let drecrement = --add
+// document.write('<p>'+"Value after Decrement:"+ drecrement+ '</p>')
+
+// let did = 3 
+// let remainder = drecrement % did
+// document.write('<p>'+"Value after Decrement:"+ remainder+ '</p>')
+
+
+// Q4:
+
+
+// let num1 = 600
+// let num2 = 5
+
+// let mul =num2*num1
+
+// document.write( '<p>'+"Total Cost  to buy  " +num2 +" Tickets to a movies  "+ "is" + " "+mul + "PKR" + '</p>');
+
+
+
+// Q5:
+
+
+// let num = 5
+// document.write( '<p>'+ "Table of "+ num + '</p>');
+// document.write( '<p> 5x1 = '  + num*1 +'</p>' );
+// document.write( '<p> 5 x 2 =' + num*2 +'</p>');
+// document.write( '<p>'+ '5 x 3 = ' + num*3+'</p>');
+// document.write( '<p>'+ '5 x 4 =' + num*4+'</p>');
+// document.write( '<p>'+ '5 x 5 =' + num*5+'</p>');
+// document.write( '<p>'+ '5 x 6 =' + num*6+'</p>');
+// document.write( '<p>'+ '5 x 7 =' + num*7+'</p>');
+// document.write( '<p>'+ '5 x 8 =' + num*8+'</p>');
+// document.write( '<p>'+ '5 x 9 =' + num*9+'</p>');
+// document.write( '<p>'+ '5 x 10 =' + num*10+'</p>');
+
+
+
+
+// Q6:
+
+
+
+
+// let f =99
+// let Calsius = (f-32)*5/9
+// document.write( '<p>'+f+"<sup>0</sup>F"+" "+"is"+" "+ Calsius +"<sup>0</sup>C"+'</p>');
+
+// let c =37
+// let fahrenheit = (c * 9/5 ) + 32
+// document.write( '<p>'+c+"<sup>0</sup>C"+" "+"is"+" "+ fahrenheit +"<sup>0</sup>F"+'</p>');
+
+
+
+// Q7:
+
+// let priceItem1 = +prompt("Enter the Price of Item 1")
+// let qtyItem1 = +prompt("Enter the Quantity of Item 1")
+// let priceItem2 = +prompt("Enter the Price of Item 2")
+// let qtyItem2 = +prompt("Enter the Quantity of Item 2")
+// const shippingCharges = 100
+
+
+// let total = (priceItem1*qtyItem1)+(priceItem2*qtyItem2)+shippingCharges
+
+// document.write( '<p>'+ "price of item1 is"+" "+priceItem1 +'</p>');
+// document.write( '<p>'+ "Quantity of item1 is"+" "+qtyItem1 +'</p>');
+// document.write( '<p>'+ "price of item2 is"+" "+priceItem2 +'</p>');
+// document.write( '<p>'+ "Quantity of item2 is "+" "+qtyItem2 +'</p>');
+// document.write( '<p>'+ "Total cost of your Order "+" "+total +'</p>');
+
+
+// Q8:
+
+// let obtMarks = +prompt("Enter the Obtain Marks")
+// let totalMarks = +prompt("Enter the Total Marks")
+// let prtg = obtMarks/totalMarks*100
+
+
+// document.write( '<p>'+"Obtain Marks: "+" "+obtMarks +'</p>');
+// document.write( '<p>'+ "Total Marks "+" "+totalMarks +'</p>');
+// document.write( '<p>'+ "percentage : "+" "+prtg +'</p>');
+
+
+
+// Q9:
+
+
+
+// const dollar = 279.50
+// const riyal = 72.5
+
+// let enterDollar = +prompt("Enter the doller")
+// let enterRiyal = +prompt("Enter the Riyal")
+
+// let cnvtRupee = (enterDollar* dollar)+(enterDollar*riyal)
+
+
+// document.write( '<h1>Currency in PKR </h1>');
+// document.write( '<p>'+ "Total currency in PKR "+" "+cnvtRupee +'</p>');
+
+
+
+// Q10:
+
+
+// let num =35 
+
+
+// num =  num + 5
+// console.log(num);
+
+// num= num*10
+// console.log(num);
+
+
+// num= num/2
+// console.log(num);
+
+
+
+// Q11:
+
+
+// let currentYear = 2024
+// let birthYear = 1998
+
+// let age = currentYear - birthYear
+
+
+// document.write( '<p>'+ "Current Year "+" "+currentYear +'</p>');
+// document.write( '<p>'+ "Birth Year "+" "+birthYear +'</p>');
+// document.write( '<p>'+ " Your Age   "+" "+age +'</p>');
+
+
+// Q12:
+
+
+// const pi = 3.142
+// let radius = +prompt('Enter the Radius of Circle')
+
+// let circunference = 2*pi*radius
+// let areaCircle = pi*radius*radius
+
+
+// document.write( '<p>'+ " Radius of Circle "+" "+radius+'</p>');
+// document.write( '<p>'+ " Circunference of Circle "+" "+circunference+'</p>');
+// document.write( '<p>'+ " Area  of Circle "+" "+areaCircle+'</p>');
+
+
+// Q13:
+
+
+// let fvtSnacks = 'choco chips'
+
+// let crrentAge = 24
+// let estiAge = 65 
+
+// let snacksPerDay = 3
+
+// let sum =(65-24)*3
+
+
+// document.write( '<h1>The Lifetime Supply Calculator </h1>');
+// document.write( '<p>'+ " Current Age : "+" "+crrentAge+'</p>');
+// document.write( '<p>'+ " Estimated Maximum Age : "+" "+estiAge+'</p>');
+// document.write( '<p>'+ " Amount of snacks (per day) : "+" "+snacksPerDay+'</p>');
+// document.write( '<p>'+ " You will need "+" "+sum+" "+"choco chips to until the ripe old age of " +estiAge  +'</p>');
+
+
