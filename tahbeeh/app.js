7e7b312bfc37fffdfd851e747d48fc2daab1e65a
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-24 17:26:08 +0500

    Add files via upload

---
+++ tahbeeh/app.js
@@ -0,0 +1,17 @@
+// chatper 12-13
+// assignment of tashbeeh conter
+
+
+let text = document.getElementById("value")
+count = 0
+
+function inc(){
+    count++
+    text.innerText = count
+}
+function reset(){
+    count = 0
+   text.innerText = count
+}
+
+
