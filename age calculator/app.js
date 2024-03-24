fa09f9362af0a5f3881a32f0dfa4a0e174a47bf0
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-03-24 16:29:16 +0500

    Add files via upload

---
+++ age calculator/app.js
@@ -0,0 +1,13 @@
+// chatper 12-13
+// assignment of age calculotor
+
+
+
+
+function ageCal(){
+    let birthYear = +prompt("Enter the birth year")
+    let currentYear = 2024
+    let age = currentYear-birthYear
+    console.log(age);
+}
+
