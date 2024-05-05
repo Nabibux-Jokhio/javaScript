b47bb3cd76bda9299f0b90588c5e578f9c3931a0
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-05-05 17:36:35 +0500

    Add files via upload

---
+++ chapter17-20-js-assignment/app.js
@@ -0,0 +1,89 @@
+// chatper 12-13
+// ARRAYS AND LOOP
+
+// Q1:
+// let std=[[],[],[]];
+
+
+// Q2:
+
+
+// Q3:
+
+// for(let i =1 ; i<=10; i++){
+//     console.log(i);
+// }
+
+
+// Q4:
+
+// let table = prompt("Enter the number you want the print the table")
+// let userLenght = prompt("Enter the lenght of table")
+
+// for(let i =1 ; i<=userLenght; i++){
+//    console.log(`${table} X  ${i} = ${table*i}`);
+// }
+
+
+// Q5:
+
+// let fruits = ["apple", "banana", "mango", "orange","strawberry"]
+
+// for(let i = 0 ; i<fruits.length ; i++){
+//     console.log(`Element at index ${i} is ${fruits[i]}`);
+// }
+
+// Q6:
+// a 
+// document.write( `<h1>Counting:</h1> `);
+// for(let i =1 ; i<=15; i++){
+//     document.write( `${i} `);
+// }
+// // b 
+// document.write( `<h1>Reverse counting:</h1> `);
+// for(let i =10 ; i>0; i--){
+//     document.write( `${i} `);
+// }
+// // c 
+// document.write( `<h1>Even:</h1> `);
+// for(let i =0 ; i<=20; i=+2){
+//     document.write( `${i} `);
+// }
+// // d 
+// document.write( `<h1>Odd:</h1> `);
+// for(let i =1 ; i<=20; i=+2){
+//     document.write( `${i} `);
+// }
+// // e 
+
+// document.write( `<h1>Series:</h1> `);
+// for(let i =2 ; i<=20; i=+2){
+//     document.write( `${i}k `);
+// }
+
+// Q7;
+
+
+
+
+
+
+// Q8:
+
+let num = [14,45,15,62,85,95,18]
+
+let largeNum = num.max()
+ 
+console.log(largeNum);
+// Q9:
+
+
+
+
+
+// Q10:
+
+// for(let i=5 ; i<=10; i=+5){
+//     document.write(`${i}`)
+// }
+
