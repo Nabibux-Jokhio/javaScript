1496fefa66d6b446b417246621a451c89503f0e1
Nabibux-Jokhio <154605738+Nabibux-Jokhio@users.noreply.github.com>
2024-05-20 23:45:15 +0500

    Add files via upload

---
+++ chapter26-30-js-assignment/app.js
@@ -0,0 +1,100 @@
+// chatper 26-30
+// MATH METHODS
+
+// Q1:
+// let pstIng = 3.452131
+
+// let roundValue = Math.round(pstIng)
+// let ceilValue = Math.ceil(pstIng)
+// let floorValue = Math.floor(pstIng)
+
+// console.log(`Number: ${pstIng}`);
+// console.log(`Round Off Value: ${roundValue}`);
+// console.log(`Floor Value: ${floorValue}`);
+// console.log(`Ceil Value: ${ceilValue}`);
+
+
+// Q2:
+// let negIng = -2.51312423
+
+// let roundValue = Math.round(negIng)
+// let ceilValue = Math.ceil(negIng)
+// let floorValue = Math.floor(negIng)
+
+// console.log(`Number: ${negIng}`);
+// console.log(`Round Off Value : ${roundValue}`);
+// console.log(`Floor Value: ${floorValue}`);
+// console.log(`Ceil Value: ${ceilValue}`);
+
+
+// Q3:
+
+
+
+// Q4:
+
+// let dice =Math.ceil(Math.random()*6)
+
+// document.write(`Random Dice Value ${dice}`);
+
+
+// Q5:
+
+// let userInput = prompt("Enter The H For Heads \n T For Tails").toUpperCase()
+// let headTailArr = ["H","T"]
+// let roundValue = Math.floor(Math.random()*headTailArr.length)
+
+
+// if(userInput === "H" || userInput === "T")  {
+//  if(userInput === headTailArr[roundValue]){
+//     document.write(`you Enter the Value ${userInput} <br>`)
+//     document.write(`${roundValue} <br>`)
+//     document.write(`Randon Coin Value: ${headTailArr[roundValue]}<br>`)
+//     document.write(`you wins`)
+//  }
+//   else {
+//     document.write(`You Enter The Value ${userInput} <br>`)
+//     document.write(`${roundValue} <br>`)
+//     document.write(`Randon Coin Value: ${headTailArr[roundValue]} <br>`)
+//     document.write(`you lose`)
+//   }
+// }
+// else{
+//     alert( `invalid`)
+// }
+
+
+
+
+// Q6:
+
+// let number = Math.ceil(Math.random()*100)
+// document.write(`Randon Number Between 1 And 100 : ${number}`);
+
+
+
+
+// Q7;
+
+
+
+
+
+
+// Q8:
+
+
+// let userNumber = +prompt(`Enter The guess the secret number \nbetween 1 to 10`)
+// let number = Math.ceil(Math.random()*10)
+
+// if (userNumber >=1 && userNumber<=10){
+// if (userNumber === number) {
+//    alert(`you guess the Right Number ${number}`) 
+// }
+// else{
+//     alert(`try again`)
+// }
+// }
+// else{
+//     alert(`invalid`) 
+// }
